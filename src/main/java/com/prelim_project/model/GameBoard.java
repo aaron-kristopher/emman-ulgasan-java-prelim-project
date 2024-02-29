278d56f49d727ec19769581da98727aa8db57f4d
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-02-29 22:07:37 +0800

    Add GameBoard class with size, board, isBoardFull(), setCell(), and clearBoard() methods

--- src/main/java/com/prelim_project/model/GameBoard.java
+++ src/main/java/com/prelim_project/model/GameBoard.java
@@ -1,5 +1,30 @@
 package com.prelim_project.model;
 
 public class GameBoard {
+    char[][] board;
+    int size;
 
+    GameBoard(int size) {
+        this.size = size;
+        board = new char[size][size];
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void setCell(int row, int col, char value) {
+        board[row][col] = value;
+    }
+
+    public void clearBoard() {
+        board = new char[size][size];
+    }
 }
