99383875ec2c5e993cf01f373dcb92516e67613d
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-02-29 22:12:37 +0800

    Add score tracking functionality to Player class

--- src/main/java/com/prelim_project/model/Player.java
+++ src/main/java/com/prelim_project/model/Player.java
@@ -2,4 +2,27 @@ package com.prelim_project.model;
 
 public class Player {
 
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void increaseScore() {
+        score++;
+    }
+
+    public void resetScore() {
+        score = 0;
+    }
 }
