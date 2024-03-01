75a06e953d706840650a393fefbe513a0ccce4d2
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-03-01 15:17:54 +0800

    Add method for switching symbol and players

--- src/main/java/com/prelim_project/game/GameLogic.java
+++ src/main/java/com/prelim_project/game/GameLogic.java
@@ -1,29 +1,28 @@
 package com.prelim_project.game;
 
 import com.prelim_project.model.GameBoard;
+import com.prelim_project.model.Player;
 
 public class GameLogic {
-    public boolean isValidMove(int row, int col, GameBoard board) {
-        if (row < 0 || row >= board.getSize() || col < 0 || col >= board.getSize()) {
+    public static boolean isValidMove(int row, int col, GameBoard board) {
+        if (row < 0 || row >= board.getSize() || col < 0 || col >= board.getSize())
             return false;
-        }
 
-        if (!board.isCellEmpty(row, col)) {
+        if (!board.isCellEmpty(row, col))
             return false;
-        }
 
         return true;
     }
 
-    public boolean checkWin(GameBoard board, char value) {
-        return true;
+    public static int checkPoints(GameBoard board, int row, int col, char value) {
+        return SOSPatternChecker.checkPoints(board, row, col, value);
     }
 
-    public boolean isGameOver(GameBoard board) {
-        if (board.isBoardFull()) {
-            return true;
-        }
-        return false;
+    public static char switchSymbol(char symbol) {
+        return symbol == 'S' ? 'O' : 'S';
     }
 
+    public static Player switchPlayer(Player[] players, Player currentPlayer) {
+        return currentPlayer == players[0] ? players[1] : players[0];
+    }
 }
