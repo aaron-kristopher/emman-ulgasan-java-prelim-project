8c7e63e314e67eb1edc380022a3ed3d1ddca34f1
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-03-01 11:46:52 +0800

    Change pattern checker implementation to StringBuffer implementation

--- src/main/java/com/prelim_project/game/SOSPatternChecker.java
+++ src/main/java/com/prelim_project/game/SOSPatternChecker.java
@@ -3,24 +3,23 @@ package com.prelim_project.game;
 import com.prelim_project.model.GameBoard;
 
 public class SOSPatternChecker {
-    private static final char[] validPattern = { 'S', 'O', 'S', 'O', 'S' };
+    private static final String onePointPattern = "SOS";
+    private static final String twoPointPattern = "SOSOS";
 
     public static int checkHorizontal(GameBoard board, int row, int col, int value) {
+        StringBuffer pattern = new StringBuffer();
         int points = 0;
-        int validPatternCount = 0;
+
         int startCol = (value == 'S') ? Math.max(0, col - 2) : Math.max(0, col - 1);
         int endCol = (value == 'S') ? Math.min(board.getSize() - 1, col + 2) : Math.min(board.getSize() - 1, col + 1);
 
         for (int i = startCol; i <= endCol; i++) {
-            if (board.getCell(row, i) == validPattern[validPatternCount])
-                validPatternCount++;
-            else if (validPatternCount <= 2)
-                validPatternCount = 0;
+            pattern.append(board.getCell(row, i));
         }
 
-        if (validPatternCount == 5)
+        if (pattern.toString().contains(twoPointPattern))
             points = 2;
-        else if (validPatternCount >= 2)
+        else if (pattern.toString().contains(onePointPattern))
             points = 1;
 
         return points;
@@ -28,20 +27,18 @@ public class SOSPatternChecker {
 
     public static int checkVertical(GameBoard board, int row, int col, int value) {
         int points = 0;
-        int validPatternCount = 0;
+        StringBuffer pattern = new StringBuffer();
+
         int startRow = (value == 'S') ? Math.max(0, row - 2) : Math.max(0, row - 1);
         int endRow = (value == 'S') ? Math.min(board.getSize() - 1, row + 2) : Math.min(board.getSize() - 1, row + 1);
 
         for (int i = startRow; i <= endRow; i++) {
-            if (board.getCell(i, col) == validPattern[validPatternCount])
-                validPatternCount++;
-            else if (validPatternCount <= 2)
-                validPatternCount = 0;
+            pattern.append(board.getCell(i, col));
         }
 
-        if (validPatternCount == 5)
+        if (pattern.toString().contains(twoPointPattern))
             points = 2;
-        else if (validPatternCount >= 2)
+        else if (pattern.toString().contains(onePointPattern))
             points = 1;
 
         return points;
