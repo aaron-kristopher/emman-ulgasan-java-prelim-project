3f0db8acc03316a4a2c2b1aa950e3bc68adaf426
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-03-01 14:22:19 +0800

    Add method to check for Diagonal Patterns

--- src/main/java/com/prelim_project/game/SOSPatternChecker.java
+++ src/main/java/com/prelim_project/game/SOSPatternChecker.java
@@ -43,4 +43,43 @@ public class SOSPatternChecker {
 
         return points;
     }
+
+    public static int checkDiagonal(GameBoard board, int row, int col, int value) {
+        int points = 0;
+        StringBuffer pattern = new StringBuffer();
+
+        int startCol = (value == 'S') ? Math.max(0, col - 2) : Math.max(0, col - 1);
+        int endCol = (value == 'S') ? Math.min(board.getSize() - 1, col + 2) : Math.min(board.getSize() - 1, col + 1);
+        int startRow = (value == 'S') ? Math.max(0, row - 2) : Math.max(0, row - 1);
+        int endRow = (value == 'S') ? Math.min(board.getSize() - 1, row + 2) : Math.min(board.getSize() - 1, row + 1);
+
+        int i = startCol;
+        int j = startRow;
+        while (i <= endCol && j <= endRow) {
+            pattern.append(board.getCell(j, i));
+            i++;
+            j++;
+        }
+
+        if (pattern.toString().contains(twoPointPattern))
+            points = 2;
+        else if (pattern.toString().contains(onePointPattern))
+            points = 1;
+
+        StringBuffer pattern2 = new StringBuffer();
+        i = endCol;
+        j = startRow;
+        while (i >= startCol && j <= endRow) {
+            pattern2.append(board.getCell(j, i));
+            i--;
+            j++;
+        }
+
+        if (pattern2.toString().contains(twoPointPattern))
+            points += 2;
+        else if (pattern2.toString().contains(onePointPattern))
+            points += 1;
+
+        return points;
+    }
 }
