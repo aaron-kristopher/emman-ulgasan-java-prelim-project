b35d4d5ef8341b4211976b06a82e0ba1faa3f588
aaron-kristopher <aaron.lim.cstr@gmail.com>
2024-03-01 16:53:41 +0800

    Add display for board and game stats

--- src/main/java/com/prelim_project/gui/GameView.java
+++ src/main/java/com/prelim_project/gui/GameView.java
@@ -1,5 +1,43 @@
 package com.prelim_project.gui;
 
+import com.prelim_project.model.GameBoard;
+import com.prelim_project.model.Player;
+
 public class GameView {
 
+    public static void printBoard(GameBoard board) {
+        int size = board.getSize();
+        System.out.print("""
+                --------------------------------------------------
+                |     |  1  |  2  |  3  |  4  |  5  |  6  |  7  |
+                --------------------------------------------------""");
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                char symbol = switch (board.getCell(i, j)) {
+                    case 'S' -> 'S';
+                    case 'O' -> 'O';
+                    default -> ' ';
+                };
+
+                if (j == 0)
+                    System.out.printf("\n|  %d  |  %s  |", i + 1, symbol);
+                else
+                    System.out.printf("  %s  |", symbol);
+            }
+            System.out.print("\n--------------------------------------------------");
+        }
+        System.out.println();
+    }
+
+    public static void printTurnInfo(Player currentPlayer, char currentSymbol) {
+        System.out.printf("Current Player: %s\tCurrent Symbol: %s", currentPlayer.getName(), currentSymbol);
+    }
+
+    public static void printScores(Player[] players) {
+        System.out.println("\n\nScores:");
+        for (Player player : players) {
+            System.out.printf("%s: %d\t", player.getName(), player.getScore());
+        }
+    }
 }
